750cd5a7ed3e598a2f59415ba0fcf625a4f882c7
Héctor <holyfiddlex@gmail.com>
2020-01-24 00:24:32 -0600

    Closeable Window

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ def main():
     pygame.init()
     win = pygame.display.set_mode((500,500))
     pygame.display.set_caption("Chess AI")
+    running = True
+    while running:
+        pygame.time.wait(100)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
