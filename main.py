b20c574f80340352ccad72e3eb759d40b0c345f4
Héctor <holyfiddlex@gmail.com>
2020-01-24 00:23:18 -0600

    Simple Pygame Window

---
+++ main.py
@@ -0,0 +1,10 @@
+import chess
+import pygame
+
+def main():
+    pygame.init()
+    win = pygame.display.set_mode((500,500))
+    pygame.display.set_caption("Chess AI")
+
+if __name__ == "__main__":
+    main()
