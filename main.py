7901b4fce4a50bd1c0d2801b36ec285730b8a003
Héctor <holyfiddlex@gmail.com>
2020-01-24 00:25:00 -0600

    Refactor

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import chess
 import pygame
 
-def main():
+def setup():
     pygame.init()
     win = pygame.display.set_mode((500,500))
     pygame.display.set_caption("Chess AI")
+    pygame.image.load("chess.png")
+
+def run():
     running = True
     while running:
         pygame.time.wait(100)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+def main():
+    setup()
+    run()
     pygame.quit()
 
 if __name__ == "__main__":
