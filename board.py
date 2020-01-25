1050621d4186292396393e36a4ea8b33f4df82ec
Héctor <hoftherose@gmail.com>
2020-01-25 11:53:42 -0600

    Get board

--- board.py
+++ board.py
@@ -1,4 +1,6 @@
+from fastcore.all import *
 import pygame
+import chess
 
 img_board = pygame.image.load("src/board.jpg")
 img_king = pygame.image.load("src/Piece_King.jpg")
@@ -7,3 +9,16 @@ img_bishop = pygame.image.load("src/Piece_Bishop.jpg")
 img_knight = pygame.image.load("src/Piece_Knight.jpg")
 img_rook = pygame.image.load("src/Piece_Rook.jpg")
 img_pawn = pygame.image.load("src/Piece_Pawn.jpg")
+
+@patch
+def get_board(x:chess.Board)->list:
+    board_rep = []
+    for square in chess.SQUARES_180:
+        row_rep = []
+        if piece := x.piece_at(square):
+            row_rep.append(piece.symbol())
+        else:
+            row_rep.append(".")
+        if chess.BB_SQUARES[square] & chess.BB_FILE_H & square == chess.H1:
+            board_rep.append(row_rep)
+    return board_rep
