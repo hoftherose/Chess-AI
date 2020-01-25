ab54046acc61663d74fee85afd459e5ae2143f14
Héctor <hoftherose@gmail.com>
2020-01-24 22:09:52 -0600

    Importing Assets

---
+++ board.py
@@ -0,0 +1,9 @@
+import pygame
+
+img_board = pygame.image.load("src/board.jpg")
+img_king = pygame.image.load("src/Piece_King.jpg")
+img_queen = pygame.image.load("src/Piece_Queen.jpg")
+img_bishop = pygame.image.load("src/Piece_Bishop.jpg")
+img_knight = pygame.image.load("src/Piece_Knight.jpg")
+img_rook = pygame.image.load("src/Piece_Rook.jpg")
+img_pawn = pygame.image.load("src/Piece_Pawn.jpg")
