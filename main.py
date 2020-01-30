788e698b531cb635393166a5ccf3a91fda361ee1
Héctor <hoftherose@gmail.com>
2020-01-29 21:51:20 -0600

    Global Board and Piece Register

--- main.py
+++ main.py
@@ -3,18 +3,21 @@ import chess
 import pygame
 import board
 
+chessboard = chess.Board()
+
 def setup():
+    global chessboard
     pygame.init()
     win = pygame.display.set_mode(WindowSize)
     win.fill(WindowColor)
     pygame.display.set_caption("Chess AI")
     icon = assets["icon"]
     pygame.display.set_icon(icon)
-    board = chess.Board()
-    rep = board.get_board()
-    board.draw_board(win)
+    rep = chessboard.get_board()
+    chessboard.draw_board(win)
 
 def run():
+    global chessboard
     running = True
     while running:
         pygame.time.wait(100)
@@ -25,7 +28,9 @@ def run():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 absx, absy = event.pos
                 if 0 <= (x:=absx-xmin) and x <= xrang and 0 <= (y:=absy-ymin) and y <= yrang:
-                    print((x//col_len,y//row_len))
+                    col, row = int(x//col_len), int(y//row_len)
+                    print(col, row)
+                    print(chessboard.get_board()[row][col])
 
 def main():
     setup()
