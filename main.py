f304fe607519f86907bfbb0a1a4a172a7cbff0ba
Héctor <hoftherose@gmail.com>
2020-02-28 17:36:47 -0600

    Game Object

--- main.py
+++ main.py
@@ -3,18 +3,7 @@ from Chess.chessboard import *
 
 chessboard = chess.Board()
 
-class Game():
-    def __init__(self, win):
-    @classmethod
-    def create(cls):
-        return cls(setupWindow())
-
-def refresh(win:pygame.Surface):
-    rep = chessboard.get_board()
-    chessboard.draw_board(win)
-    pygame.display.update()
-
-def setup():
+def setupWindow():
     pygame.init()
     win = pygame.display.set_mode(WindowSize)
     win.fill(WindowColor)
@@ -22,27 +11,39 @@ def setup():
     icon = assets["icon"]
     pygame.display.set_icon(icon)
     chessboard.selected = None
-    refresh(win)
     return win
 
-def run(win:pygame.Surface):
-    running = True
-    while running and not(chessboard.is_checkmate()):
-        pygame.time.wait(100)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                absx, absy = event.pos
-                if 0 <= (x:=absx-xmin) and x <= xrang and 0 <= (y:=absy-ymin) and y <= yrang:
-                    col, row = int(x//col_len), int(y//row_len)
-                    chessboard.select(col,row)
-        refresh(win)
+class Game():
+    def __init__(self, win):
+        self.win = win
+        self.refresh()
+
+    @classmethod
+    def create(cls):
+        return cls(setupWindow())
+
+    def refresh(self):
+        rep = chessboard.get_board()
+        chessboard.draw_board(self.win)
+        pygame.display.update()
+
+    def run(self):
+        running = True
+        while running and not(chessboard.is_checkmate()):
+            pygame.time.wait(100)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    absx, absy = event.pos
+                    if 0 <= (x:=absx-xmin) and x <= xrang and 0 <= (y:=absy-ymin) and y <= yrang:
+                        col, row = int(x//col_len), int(y//row_len)
+                        chessboard.select(col,row)
+            self.refresh()
 
 def main():
-    win = setup()
-    run(win)
-    pygame.quit()
+    game = Game.create()
+    game.run()
 
 if __name__ == "__main__":
     main()
