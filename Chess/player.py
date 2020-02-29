65a0fad0166e44400f7763a21195b1fa25ec9c35
Héctor <hoftherose@gmail.com>
2020-02-28 18:01:50 -0600

    Player Class

---
+++ Chess/player.py
@@ -0,0 +1,12 @@
+from .tools import *
+from datetime import timedelta
+
+class Player():
+    def __init__(self, name:str, color:bool, time:int=1800, elo:int=1200):
+        store_attr(self, "name,color,time,elo")
+    
+    def __str__(self):
+        return self.name
+    
+    def __repr__(self):
+        return f"Player Name: {self.name} with {self.elo} ELO, playing {'whites' if self.color else 'blacks'}. Time remaining {timedelta(seconds=self.time)}."
